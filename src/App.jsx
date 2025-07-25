fc69191d0f4b0affc233333251ee0a6cac36ec3c
Utkarsh Mishra <utkarshmishra048@gmail.com>
2025-07-25 10:39:24 +0530

    Minor change

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
             onClick={handleLogout}
             className="bg-red-500 text-white px-4 py-2 rounded float-right"
           >
-            Logout
+            Logouts
           </button>
         )}
 
